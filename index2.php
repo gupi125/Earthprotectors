60823052b11f538b079b23e0fa2e7f28045e4cd2
gupi125 <40681821+gupi125@users.noreply.github.com>
2020-03-21 17:09:39 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -131,7 +131,7 @@ if ($get_user->num_rows == 1)
                   <a href="userprofileC.php?username=<?php echo $_SESSION['username'] ?>">View Profile</a>      
                     <a href="editprofileViewC.php?username=<?php echo $_SESSION['username'] ?>">Edit Profile</a>      
                     <a href="addMaterial.php">Choose Material</a>
-                    <a href="addedMaterial.php">Added Material</a>
+                    <a href="addedMaterial.php">View added Material</a>
                   </div>
                 </div></li>
                 
